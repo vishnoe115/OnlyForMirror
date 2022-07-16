d2bf935cfec5290b42e9001cfe43c69255c6cd6e
arshsisodiya <arshsisodiya@gmail.com>
2022-07-16 23:25:51 +0530

    fix

--- bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
+++ bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
@@ -8,7 +8,7 @@ from pyrogram import Client, enums
 
 from bot import DOWNLOAD_DIR, AS_DOCUMENT, AS_DOC_USERS, AS_MEDIA_USERS, CUSTOM_FILENAME, \
                  EXTENSION_FILTER, app, LEECH_LOG, BOT_PM, rss_session, TG_SPLIT_SIZE
-from bot.helper.ext_utils.fs_utils import take_ss, get_media_info, get_video_resolution, get_path_size
+from bot.helper.ext_utils.fs_utils import take_ss, get_media_info, get_path_size
 from bot.helper.ext_utils.bot_utils import get_readable_file_size
 
 LOGGER = getLogger(__name__)
@@ -96,7 +96,8 @@ class TgUploader:
                         img = Image.open(thumb)
                         width, height = img.size
                     else:
-                        width, height = get_video_resolution(up_path)
+                        width = 480
+                        height = 320
                     if not file_.upper().endswith(("MKV", "MP4")):
                         file_ = ospath.splitext(file_)[0] + '.mp4'
                         new_path = ospath.join(dirpath, file_)
