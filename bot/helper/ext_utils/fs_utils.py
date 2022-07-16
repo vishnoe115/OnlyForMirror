12edd62d7a30635fdd66f3eececadbac5230f59a
arshsisodiya <arshsisodiya@gmail.com>
2022-07-17 00:51:29 +0530

    Fixing 4 GB Split

--- bot/helper/ext_utils/fs_utils.py
+++ bot/helper/ext_utils/fs_utils.py
@@ -138,8 +138,8 @@ def split_file(path, size, file_, dirpath, split_size, start_time=0, i=1, inLoop
             srun(["ffmpeg", "-hide_banner", "-loglevel", "error", "-ss", str(start_time),
                   "-i", path, "-fs", str(split_size), "-map", "0", "-c", "copy", out_path])
             out_size = get_path_size(out_path)
-            if out_size > 2097152000:
-                dif = out_size - 2097152000
+            if out_size > 4194304000:
+                dif = out_size - 4194304000
                 split_size = split_size - dif + 3000000
                 osremove(out_path)
                 return split_file(path, size, file_, dirpath, split_size, start_time, i, True)
